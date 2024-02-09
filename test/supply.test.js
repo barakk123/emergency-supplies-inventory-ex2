2b557de81818f5d9e55e97b572f5c1759d01234e
Barak Daniel <71881281+barakk123@users.noreply.github.com>
2024-02-09 16:35:09 +0200

    Add files via upload

--- test/supply.test.js
+++ test/supply.test.js
@@ -15,105 +15,106 @@ beforeAll(async () => {
 describe('Supplies API Endpoints', () => {
     describe('GET /supplies', () => {
         it('returns all supplies', async () => {
-          const mockSupplies = [{ supply_name: 'Water', category: 'Core', unit_price: 2.80, supplier: "Sami", location: "Israel", quantity: 100 }, { supply_name: 'Bandage', category: 'Medic', unit_price: 3.80, supplier: "Shimi", location: "Israel", quantity: 10 }];
-          emergencySupplier.getAllSupplies.mockResolvedValue(mockSupplies);
-      
-          const response = await request(app).get('/supplies');
-          
-          expect(response.statusCode).toBe(200);
-          expect(response.body).toEqual(mockSupplies);
-          expect(emergencySupplier.getAllSupplies).toHaveBeenCalledTimes(1);
+            const mockSupplies = [{ supply_name: 'Water', category: 'Core', unit_price: 2.80, supplier: "Sami", location: "Israel", quantity: 100 }, { supply_name: 'Bandage', category: 'Medic', unit_price: 3.80, supplier: "Shimi", location: "Israel", quantity: 10 }];
+            emergencySupplier.getAllSupplies.mockResolvedValue(mockSupplies);
+        
+            const response = await request(app).get('/supplies');
+            
+            expect(response.statusCode).toBe(200);
+            expect(response.body).toEqual(mockSupplies);
+            expect(emergencySupplier.getAllSupplies).toHaveBeenCalledTimes(1);
         });
 
-    it('returns 404 when no supplies are found', async () => {
-        emergencySupplier.getAllSupplies.mockResolvedValue([]);
-        const response = await request(app).get('/supplies');
-        expect(response.statusCode).toBe(404);
-        expect(response.text).toBe('No supplies found.');
+        it('returns 404 when no supplies are found', async () => {
+            emergencySupplier.getAllSupplies.mockResolvedValue([]);
+            const response = await request(app).get('/supplies');
+            expect(response.statusCode).toBe(404);
+            expect(response.text).toBe('No supplies found.');
 
+        });
+
+        it('returns 500 when an error occurs', async () => {
+            emergencySupplier.getAllSupplies.mockRejectedValue(new ServerError());
+            const response = await request(app).get('/supplies');
+            expect(response.statusCode).toBe(500);
+            expect(response.body.error).toBe('Internal Server Error');
+            
+        });
     });
 
-    it('returns 500 when an error occurs', async () => {
-        emergencySupplier.getAllSupplies.mockRejectedValue(new ServerError());
-        const response = await request(app).get('/supplies');
-        expect(response.statusCode).toBe(500);
-        expect(response.body.error).toBe('Internal Server Error');
-        
-    });
-  });
+    describe('GET /supplies/:supplyName', () => {
+        it('returns a supply by supplyName', async () => {
+            const mockSupply = { supply_name: 'Water', category: 'Core', unit_price: 2.80, supplier: "Sami", location: "Israel", quantity: 100 };
+            emergencySupplier.getSupplyByName.mockResolvedValue(mockSupply);
+            const response = await request(app).get('/supplies/Water');
+            expect(response.statusCode).toBe(200);
+            expect(response.body).toEqual(mockSupply);
+            expect(emergencySupplier.getSupplyByName).toHaveBeenCalledWith('Water');
+        });
 
-  describe('GET /supplies/:supplyName', () => {
-    it('returns a supply by supplyName', async () => {
-        const mockSupply = { supply_name: 'Water', category: 'Core', unit_price: 2.80, supplier: "Sami", location: "Israel", quantity: 100 };
-        emergencySupplier.getSupplyByName.mockResolvedValue(mockSupply);
-        const response = await request(app).get('/supplies/Water');
-        expect(response.statusCode).toBe(200);
-        expect(response.body).toEqual(mockSupply);
-        expect(emergencySupplier.getSupplyByName).toHaveBeenCalledWith('Water');
-    });
+        it('returns 404 if the supply not found', async () => {
+            emergencySupplier.getSupplyByName.mockResolvedValue(null);
+            const response = await request(app).get('/supplies/Water');
+            expect(response.statusCode).toBe(404);
+            expect(response.text).toBe('Supply not found');
+        });
 
-    it('returns 404 if the supply not found', async () => {
-        emergencySupplier.getSupplyByName.mockResolvedValue(null);
-        const response = await request(app).get('/supplies/Water');
-        expect(response.statusCode).toBe(404);
-        expect(response.text).toBe('Supply not found');
-    });
+        /*it('returns 400 if supplyName is not provided', async () => {
+            const response = await request(app).get('/supplies/');
+            expect(response.statusCode).toBe(400);
+            expect(response.text).toBe('Supply name is required');
+            
+        });*/
 
-    it('returns 400 if supplyName is not provided', async () => {
-        const response = await request(app).get('/supplies/');
-        expect(response.statusCode).toBe(400);
-        expect(response.text).toBe('Supply name is required');
-        
-    });
+        it('returns 500 when an error occurs', async () => {
+            emergencySupplier.getSupplyByName.mockRejectedValue(new ServerError());
+            const response = await request(app).get('/supplies/Water');
+            expect(response.statusCode).toBe(500);
+            expect(response.body.error).toBe('Internal Server Error');
 
-    it('returns 500 when an error occurs', async () => {
-        emergencySupplier.getSupplyByName.mockRejectedValue(new ServerError());
-        const response = await request(app).get('/supplies/Water');
-        expect(response.statusCode).toBe(500);
-        expect(response.body.error).toBe('Internal Server Error');
-
-    });
-  });
-
-  describe('POST /supplies', () => {
-    it('creates a new supply', async () => {
+        });
     });
 
-    it('returns 400 if supply info is not provided', async () => {
+    describe('POST /supplies', () => {
+        it('creates a new supply', async () => {
+
+        });
+
+        it('returns 400 if supply info is not provided', async () => {
+        });
+
+        it('returns 409 if conflict on supplyName', async () => {
+        });
+
+        it('returns 500 when an error occurs', async () => {
+        });
     });
 
-    it('returns 409 if conflict on supplyName', async () => {
+    describe('PUT /supplies/:supplyName', () => {
+        it('updates a supply', async () => {
+        });
+
+        it('returns 400 if supplyName is not provided or if supply invalid data', async () => {
+        });
+
+        it('returns 409 if conflict on supplyName', async () => {
+        });
+
+        it('returns 404 if supply not found', async () => {
+        });
+
+        it('returns 500 when an error occurs', async () => {
+        });
     });
 
-    it('returns 500 when an error occurs', async () => {
-    });
-  });
+    describe('DELETE /supplies/:supplyName', () => {
+        it('deletes a supply', async () => {
+        });
 
-  describe('PUT /supplies/:supplyName', () => {
-    it('updates a supply', async () => {
-    });
+        it('returns 404 if supply not found', async () => {
+        });
 
-    it('returns 400 if supplyName is not provided or if supply invalid data', async () => {
+        it('returns 500 when an error occurs', async () => {
+        });
     });
-
-    it('returns 409 if conflict on supplyName', async () => {
-    });
-
-    it('returns 404 if supply not found', async () => {
-    });
-
-    it('returns 500 when an error occurs', async () => {
-    });
-  });
-
-  describe('DELETE /supplies/:supplyName', () => {
-    it('deletes a supply', async () => {
-    });
-
-    it('returns 404 if supply not found', async () => {
-    });
-
-    it('returns 500 when an error occurs', async () => {
-    });
-  });
 });
